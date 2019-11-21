a992ffb3ee84352948ae0717c0dc83b0b23dab64
itzel <izzy1984@byui.edu>
2019-11-21 12:37:27 -0800

    tm

--- lesson-9/js/onefiletorulethemall.js
+++ lesson-9/js/onefiletorulethemall.js
@@ -1,7 +1,20 @@
+//day&time
+var day = new Date();
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
 
-function date(){
-const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hours: 'numeric', minutes: 'numeric', seconds: 'numeric'};
-document.getElementById("currentdate").textContent= new Date().toLocaleDateString('en-US', options);}
+var today = weekday[day.getDay()];
+if(today=="Friday"){
+  //  console.log("I'll make you banana pancakes")
+  document.getElementById("pancakes").innerHTML ="Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion." ;
+}
+
 
 //windchill
 
@@ -59,23 +72,12 @@ WebFont.load({ google: {families: [ 'Slabo 27px']}});
       });
     }
   }
-//day&time
-  var day = new Date();
-  var weekday = new Array(7);
-  weekday[0] = "Sunday";
-  weekday[1] = "Monday";
-  weekday[2] = "Tuesday";
-  weekday[3] = "Wednesday";
-  weekday[4] = "Thursday";
-  weekday[5] = "Friday";
-  weekday[6] = "Saturday";
-  
-  var today = weekday[day.getDay()];
-  if(today=="Friday"){
-    //  console.log("I'll make you banana pancakes")
-    document.getElementById("pancakes").innerHTML ="Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion." ;
-  }
   //form slider
   function adjustRating(rating) {
     document.getElementById("ratingvalue").innerHTML = rating;
 }
+//footer
+function date(){
+  const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric', hours: 'numeric', minutes: 'numeric', seconds: 'numeric'};
+  document.getElementById("currentdate").textContent= new Date().toLocaleDateString('en-US', options);}
+  
