e90058084657255f9469dab4c0972747c85242a8
itzel <izzy1984@byui.edu>
2020-04-08 15:57:33 -0700

    wednesday

---
+++ oud/js/fontloading.js
@@ -0,0 +1,26 @@
+WebFont.load({
+    google: {
+      families: [
+         'Handlee'
+      ]
+    }
+  });
+
+  WebFont.load({
+    google: {
+      families: [
+        'Nanum Gothic'
+      ]
+    }
+  });
+ 
+ 
+  WebFont.load({
+    google: {
+      families: [
+         'Ubuntu'
+      ]
+    }
+  });
+ 
+  
